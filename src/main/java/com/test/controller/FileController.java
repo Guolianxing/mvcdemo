cf0c884e50ffd6117dbb54c50e465d5ff2bc33ba
Guolianxing <2944357797@qq.com>
2018-08-23 10:39:29 +0800

    mvc上传下载文件

--- src/main/java/com/test/controller/FileController.java
+++ src/main/java/com/test/controller/FileController.java
@@ -1,11 +1,17 @@
 package com.test.controller;
 
 import com.test.dto.UserDto;
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.ContextLoader;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -22,7 +28,7 @@ public class FileController {
     @RequestMapping(value = "/upload")
     @ResponseBody
     // 上传文件的入参需要是表单中的name，而不是文件名
-    // 如果不用表单的name属性，则需要@RequestParam(value = "xxx")注解
+    // 如果不用表单的name属性，则需要@RequestParam(value = "name")注解
     // 有除文件以外的表单参数，只需name对应即可
     public String uploadFile(MultipartFile file) {
         String path = ContextLoader.getCurrentWebApplicationContext().getServletContext().getRealPath("upload");
@@ -103,5 +109,26 @@ public class FileController {
             e.printStackTrace();
         }
         return "fail";
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    // 捕获文件大小异常
+    public String doException(Exception e) {
+        if (e instanceof MaxUploadSizeExceededException) {
+            System.out.println("文件大小超过限制" + ((MaxUploadSizeExceededException) e).getMaxUploadSize());
+            return "size exceeded";
+        }
+        return "no error";
+    }
+
+    @RequestMapping(value = "download")
+    public ResponseEntity<byte[]> download() throws Exception {
+        String path = ContextLoader.getCurrentWebApplicationContext().getServletContext().getRealPath("imgs/timg.jpg");
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDispositionFormData("attachment", file.getName());
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
     }
 }
